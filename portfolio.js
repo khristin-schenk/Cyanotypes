9c223e74ac35ac416594a4992154b675132f5cc0
Khristin <104738808+khristin-schenk@users.noreply.github.com>
2026-07-12 05:12:24 -0600

    Update and rename artwork.js to portfolio.js

--- portfolio.js
+++ portfolio.js
@@ -1,138 +1,135 @@
-const artworks = [
+const artwork = [
 
 {
 title:"Red 01",
-medium:"Cyanotype",
-category:"Printmaking",
-year:2025,
+category:"cyanotype",
 image:"https://raw.githubusercontent.com/khristin-schenk/Cyanotypes/main/2025-2026/IMG_0023_Red01.png"
 },
 
 {
 title:"Red 02",
-medium:"Cyanotype",
-category:"Printmaking",
-year:2025,
+category:"cyanotype",
 image:"https://raw.githubusercontent.com/khristin-schenk/Cyanotypes/main/2025-2026/IMG_0021_Red02.png"
 },
 
 {
 title:"Red 03",
-medium:"Cyanotype",
-category:"Printmaking",
-year:2025,
+category:"cyanotype",
 image:"https://raw.githubusercontent.com/khristin-schenk/Cyanotypes/main/2025-2026/IMG_0029_Red03.png"
 },
 
 {
 title:"Red 04",
-medium:"Cyanotype",
-category:"Printmaking",
-year:2025,
+category:"cyanotype",
 image:"https://raw.githubusercontent.com/khristin-schenk/Cyanotypes/main/2025-2026/IMG_0026_Red04.png"
 },
 
 {
 title:"Red 05",
-medium:"Cyanotype",
-category:"Printmaking",
-year:2025,
+category:"cyanotype",
 image:"https://raw.githubusercontent.com/khristin-schenk/Cyanotypes/main/2025-2026/IMG_0022_Red05.png"
 },
 
 {
 title:"Red 06",
-medium:"Cyanotype",
-category:"Printmaking",
-year:2025,
+category:"cyanotype",
 image:"https://raw.githubusercontent.com/khristin-schenk/Cyanotypes/main/2025-2026/IMG_0028_Red06.png"
 },
 
 {
 title:"Red 07",
-medium:"Cyanotype",
-category:"Printmaking",
-year:2025,
+category:"cyanotype",
 image:"https://raw.githubusercontent.com/khristin-schenk/Cyanotypes/main/2025-2026/IMG_0010_Red07.png"
 },
 
 {
 title:"Red 08",
-medium:"Cyanotype",
-category:"Printmaking",
-year:2025,
+category:"cyanotype",
 image:"https://raw.githubusercontent.com/khristin-schenk/Cyanotypes/main/2025-2026/IMG_0011_Red08.png"
 },
 
 {
 title:"Yellow 01",
-medium:"Cyanotype",
-category:"Printmaking",
-year:2025,
+category:"cyanotype",
 image:"https://raw.githubusercontent.com/khristin-schenk/Cyanotypes/main/2025-2026/IMG_0012_Yellow-01.png"
 },
 
 {
 title:"Blue 01",
-medium:"Cyanotype",
-category:"Printmaking",
-year:2025,
+category:"cyanotype",
 image:"https://raw.githubusercontent.com/khristin-schenk/Cyanotypes/main/2025-2026/IMG_0016_Blue-01.png"
 },
 
 {
 title:"Blue 02",
-medium:"Cyanotype",
-category:"Printmaking",
-year:2025,
+category:"cyanotype",
 image:"https://raw.githubusercontent.com/khristin-schenk/Cyanotypes/main/2025-2026/IMG_0015_Blue-02.png"
 },
 
 {
 title:"Blue 03",
-medium:"Cyanotype",
-category:"Printmaking",
-year:2025,
+category:"cyanotype",
 image:"https://raw.githubusercontent.com/khristin-schenk/Cyanotypes/main/2025-2026/IMG_0017_Blue-03.png"
 },
 
 {
 title:"Violet 01",
-medium:"Cyanotype",
-category:"Printmaking",
-year:2025,
+category:"cyanotype",
 image:"https://raw.githubusercontent.com/khristin-schenk/Cyanotypes/main/2025-2026/IMG_0027_Violet-01.png"
 },
 
 {
 title:"Violet 02",
-medium:"Cyanotype",
-category:"Printmaking",
-year:2025,
+category:"cyanotype",
 image:"https://raw.githubusercontent.com/khristin-schenk/Cyanotypes/main/2025-2026/IMG_0024_Violet-02.png"
 },
 
 {
 title:"Violet 03",
-medium:"Cyanotype",
-category:"Printmaking",
-year:2025,
+category:"cyanotype",
 image:"https://raw.githubusercontent.com/khristin-schenk/Cyanotypes/main/2025-2026/IMG_0019_Violet-03.png"
 },
 
 {
 title:"Violet 04",
-medium:"Cyanotype",
-category:"Printmaking",
-year:2025,
+category:"cyanotype",
 image:"https://raw.githubusercontent.com/khristin-schenk/Cyanotypes/main/2025-2026/IMG_0020_Violet-04.png"
 },
 
 {
 title:"Violet 05",
-medium:"Cyanotype",
-category:"Printmaking",
-year:2025,
+category:"cyanotype",
 image:"https://raw.githubusercontent.com/khristin-schenk/Cyanotypes/main/2025-2026/IMG_0025_Violet-05.png"
+},
+
+{
+title:"Cut Paper 01",
+category:"cutpaper",
+image:"https://raw.githubusercontent.com/khristin-schenk/cut-paper-compositions/main/Cut%20Paper%20Compositions/2026-27/Cut-Paper-01.png"
+},
+
+{
+title:"Cut Paper 02",
+category:"cutpaper",
+image:"https://raw.githubusercontent.com/khristin-schenk/cut-paper-compositions/main/Cut%20Paper%20Compositions/2026-27/Cut-Paper-02.png"
+},
+
+{
+title:"Cut Paper 03",
+category:"cutpaper",
+image:"https://raw.githubusercontent.com/khristin-schenk/cut-paper-compositions/main/Cut%20Paper%20Compositions/2026-27/Cut-Paper-03.png"
+},
+
+{
+title:"Cut Paper 04",
+category:"cutpaper",
+image:"https://raw.githubusercontent.com/khristin-schenk/cut-paper-compositions/main/Cut%20Paper%20Compositions/2026-27/Cut-Paper-04.png"
+},
+
+{
+title:"Cut Paper 05",
+category:"cutpaper",
+image:"https://raw.githubusercontent.com/khristin-schenk/cut-paper-compositions/main/Cut%20Paper%20Compositions/2026-27/Cut-Paper-05.png"
 }
+
 ];
